36f93ec03c3afdda516d623d6267eab14db698f7
Shirsho Dipto <shirshodipto@gmail.com>
2022-11-20 18:55:32 +0600

    Write update function

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,23 @@
-console.log("it is working");
+
+
+const gameBoard = (() => {
+    array = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
+    return {array};
+})();
+
+
+function updateBoard() {
+    for (let index = 0; index < gameBoard.array.length; index++) {
+        if (gameBoard.array[index] === 'x') {
+            const x = document.querySelector(`div[index="${index}"] .x-icon`);
+            x.setAttribute('id', 'inserted');
+        }
+        else if (gameBoard.array[index] === 'o') {
+            const o = document.querySelector(`div[index="${index}"] .o-icon`);
+            o.setAttribute('id', 'inserted');
+        }
+    }
+}
+
+
+updateBoard();
